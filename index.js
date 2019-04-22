477cd3b8a2bd9532d358e03739dd87029379ebf6
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-04-22 15:39:35 -0400

    Initial setup

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => res.send("It's working"));
+
+server.listen(5000, () => {
+  console.log('*** API running on port 5000 ***');
+});
