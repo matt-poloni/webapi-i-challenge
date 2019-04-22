c619bded3c907bf1efab16a05b6f95b5279a1e68
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-04-22 17:22:28 -0400

    Should complete MVP by completing PUT by ID

--- index.js
+++ index.js
@@ -38,12 +38,26 @@ server.get('/api/users/:id', (req, res) => {
 server.delete('/api/users/:id', (req, res) => {
   const userID = req.params.id;
   db.remove(userID)
-    .then(user => {
-      !user
-        ? res.status(404).json({ message: 'The user with the specified ID does not exist.' })
-        : res.status(204).end();
-    })
-    .catch(err => res.status(500).json({ error: 'The user could not be removed.' }))
+  .then(user => {
+    !user
+    ? res.status(404).json({ message: 'The user with the specified ID does not exist.' })
+    : res.status(204).end();
+  })
+  .catch(err => res.status(500).json({ error: 'The user could not be removed.' }))
+})
+
+server.put('/api/users/:id', (req, res) => {
+  const newUser = req.body;
+  const userID = req.params.id;
+  !newUser.name || !newUser.bio
+    ? res.status(400).json({ errorMessage: 'Please provide name and bio for the user.' })
+    : db.update(userID, newUser)
+        .then(user => {
+          !user
+            ? res.status(404).json({ message: 'The user with the specified ID does not exist.' })
+            : res.status(200).json(user);
+        })
+        .catch(err => res.status(500).json({ error: 'The user information could not be modified.' }))
 })
 
 server.listen(5000, () => {
